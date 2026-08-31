aa6e989fc322bbf74ef0cb3d2c644b8a1c7f64f2
Vogar <vogarabad8160528@gmail.com>
2026-08-31 16:28:34 -0500

    Initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HelpDeskWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
