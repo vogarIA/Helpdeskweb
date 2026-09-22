baf0ff635b82b36f7cfd58f93e91cf40b61e5538
Vogar <vogarabad8160528@gmail.com>
2026-09-21 23:57:27 -0600

    Agregar páginas restringidas por rol (Admin y User)

--- Program.cs
+++ Program.cs
@@ -1,12 +1,49 @@
+using HelpDeskWeb.Commands;
+using HelpDeskWeb.Data;
+using HelpDeskWeb.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+builder.Services.AddIdentity<Usser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 12;
+
+    options.User.RequireUniqueEmail = true;
+    options.SignIn.RequireConfirmedAccount = false;
+    options.SignIn.RequireConfirmedEmail = false;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+})
+.AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+// Interceptar comandos ANTES de levantar el servidor
+using (var scope = app.Services.CreateScope())
+{
+    bool wasCommand = await CommandRunner.TryRunAsync(args, scope.ServiceProvider);
+    if (wasCommand)
+    {
+        return; // termina el programa sin levantar el sitio web
+            }
+    }
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
